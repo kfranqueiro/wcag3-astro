2deee82e37cc94f0fac1ebfc7ef693f04853cc9a
Kenneth G. Franqueiro <kfranqueiro@users.noreply.github.com>
2025-01-27 14:39:38 -0500

    Support multiple elements + lists in description; fix EntryText logic

--- src/lib/unifiedjs/guidelines.ts
+++ src/lib/unifiedjs/guidelines.ts
@@ -1,10 +1,22 @@
 import type { RehypePlugin, RemarkPlugin } from "@astrojs/markdown-remark";
+import type { RootContent } from "hast";
 
 import { toHtml } from "hast-util-to-html";
 import { visit } from "unist-util-visit";
 import type { VFile } from "vfile";
 
-const isGuidelineFile = (file: VFile) => file.dirname?.startsWith(`${file.cwd}/guidelines/groups`);
+const groupsPath = `guidelines/groups`;
+const isGuidelineFile = (file: VFile) => file.dirname?.startsWith(`${file.cwd}/${groupsPath}`);
+
+function getGuidelineFileType(file: VFile) {
+  if (!isGuidelineFile(file)) return null;
+  const remainingPath = file.dirname!.replace(`${file.cwd}/${groupsPath}/`, "");
+  const segments = remainingPath?.split("/");
+  if (segments.length === 0) return "group";
+  if (segments.length === 1) return "guideline";
+  if (segments.length === 2) return "requirement";
+  return null;
+}
 
 const getFrontmatter = (file: VFile) => file.data.astro!.frontmatter!;
 
@@ -32,22 +44,34 @@ const customDirectives: RemarkPlugin = () => (tree, file) => {
   });
 };
 
-/** Extracts the first paragraph's HTML to a separate value. */
-const extractFirstParagraph: RehypePlugin = () => (tree, file) => {
+/** Extracts leading paragraphs/lists' HTML to a separate value. */
+const extractLeadingContent: RehypePlugin = () => (tree, file) => {
   if (!isGuidelineFile(file)) return;
-  const firstChild = tree.children[0];
+  const contentTags = ["p", "ul", "ol"];
+  const leadingElements: RootContent[] = [];
+  const textClass = `${getGuidelineFileType(file)}-text`;
 
-  if (firstChild.type === "element" && firstChild.tagName === "p") {
-    const html = toHtml(firstChild, {
-      allowDangerousCharacters: true,
-      allowDangerousHtml: true,
-    });
-    getFrontmatter(file).description = html.replace(/^<p>(.*)<\/p>$/, "$1");
-    tree.children.shift();
-  } else {
-    file.fail("Leading paragraph expected but not found.");
+  for (
+    let child = tree.children[0];
+    child?.type === "text" || (child?.type === "element" && contentTags.includes(child.tagName));
+    tree.children.shift() && (child = tree.children[0])
+  ) {
+    if (child.type === "element") {
+      // TODO: Should this class not apply to lists?
+      const existingClass = child.properties.class;
+      if (existingClass) child.properties.class += ` ${textClass}`;
+      else child.properties.class = textClass;
+    }
+    leadingElements.push(child);
   }
+  if (!leadingElements.length) file.fail("Leading content expected but not found.");
+
+  const html = toHtml(leadingElements, {
+    allowDangerousCharacters: true,
+    allowDangerousHtml: true,
+  });
+  getFrontmatter(file).description = html;
 };
 
 export const guidelinesRemarkPlugins = [customDirectives];
-export const guidelinesRehypePlugins = [extractFirstParagraph];
+export const guidelinesRehypePlugins = [extractLeadingContent];
