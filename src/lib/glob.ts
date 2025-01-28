073f76a6cdb64b5ce91ada1dd6a7981865004f55
Kenneth G. Franqueiro <kfranqueiro@users.noreply.github.com>
2025-01-28 06:32:18 -0500

    Remove unneeded code since MDX removal

--- src/lib/glob.ts
+++
@@ -1,9 +0,0 @@
-import { basename } from "path";
-
-/** Takes a hash returned from import.meta.glob, and shortens its keys to basenames only. */
-export function mapImportsToBasenames<M>(globResult: Record<string, M>) {
-  return Object.entries(globResult).reduce((map, [key, value]) => {
-    map[basename(key, ".astro")] = value;
-    return map;
-  }, {} as typeof globResult);
-}
